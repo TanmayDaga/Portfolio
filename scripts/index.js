597f6447ad66290e9e1b3cc7768cf32ede89207d
tanmaydaga <tanmaydaga06@gmail.com>
2023-10-12 15:58:04 +0530

    changed splash screen time

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,21 @@
-function removeFadeOut(element,speed){
-    element.style.transition = "opacity "+(speed/1000)+"s ease"
-  
-    setTimeout(()=>element.parentNode.removeChild(element),12000);
+function removeFadeOut(element, speed) {
+  element.style.transition = "opacity " + speed / 1000 + "s ease";
+
+  setTimeout(() => element.parentNode.removeChild(element), 12000);
 }
-removeFadeOut(document.getElementById("body-splash"),10000)
+removeFadeOut(document.getElementById("body-splash"), 5000);
 
-
-const pythonProjectsContainer=document.querySelector("#python-projects");
+const pythonProjectsContainer = document.querySelector("#python-projects");
 const androidProjectsContainer = document.querySelector("#android-projects");
 
-
-
 projects.python.forEach((element) => {
-    pythonProjectsContainer.innerHTML += `
+  pythonProjectsContainer.innerHTML += `
     <img src=${element.image} class = "dC3nv" alt ="Image not found" style="height: 100%;" onclick="window.open('${element.url}','_blank')">
     `;
-})
+});
 
-projects.android.forEach((element) =>{
-    androidProjectsContainer.innerHTML += `
+projects.android.forEach((element) => {
+  androidProjectsContainer.innerHTML += `
     <img src="${element.image}" class = "dC3nv" alt ="Image not found" style="height: 100%;" onclick="window.open('${element.url}','_blank')">
       `;
-})
+});
